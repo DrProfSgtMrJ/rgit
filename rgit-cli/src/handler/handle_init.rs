8f5aff588e942b84180e763264ba66120fc835cd
DrProfSgtMrJ <bacon388@gmail.com>
2025-10-09 18:13:02 -0700

    creating head file

--- rgit-cli/src/handler/handle_init.rs
+++ rgit-cli/src/handler/handle_init.rs
@@ -1,12 +1,20 @@
-use std::{fs, path::PathBuf};
+use std::{
+    fs::{self, File},
+    path::PathBuf,
+};
 
+const HEAD_FILE_NAME: &str = "HEAD";
 pub fn handle_init(name: PathBuf, description: Option<String>) -> Result<(), String> {
     // Create a directory with the name
-    println!("Works: {:?}", name);
-
     fs::create_dir_all(name.as_path())
         .map_err(|e| format!("failed to create directory {:?}: {}", name, e))?;
 
-    println!("Initialized repo {:?}", name);
+    // Creates HEAD file
+    let mut head_file = PathBuf::from(name.clone());
+    head_file.push(HEAD_FILE_NAME);
+
+    let _ =
+        File::create(head_file).map_err(|e| format!("failed to create Head file in {:?}", name))?;
+    println!("Initialized repo {:?}", name.clone());
     return Ok(());
 }
