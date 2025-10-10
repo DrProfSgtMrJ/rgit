fc2931fab186d9219a99201ba3cf60fe4fad094b
DrProfSgtMrJ <bacon388@gmail.com>
2025-10-09 18:22:16 -0700

    fixing file and dir building

--- rgit-cli/src/handler/handle_init.rs
+++ rgit-cli/src/handler/handle_init.rs
@@ -3,18 +3,27 @@ use std::{
     path::PathBuf,
 };
 
+const RGIT_DIR_NAME: &str = ".rgit";
 const HEAD_FILE_NAME: &str = "HEAD";
+const CONFIG_FILE_NAME: &str = "config";
+
 pub fn handle_init(name: PathBuf, description: Option<String>) -> Result<(), String> {
     // Create a directory with the name
-    fs::create_dir_all(name.as_path())
-        .map_err(|e| format!("failed to create directory {:?}: {}", name, e))?;
+    let rgit_dir = name.join(RGIT_DIR_NAME);
+    fs::create_dir_all(rgit_dir.as_path())
+        .map_err(|e| format!("failed to create directory in {:?}: {}", name, e))?;
 
     // Creates HEAD file
-    let mut head_file = PathBuf::from(name.clone());
-    head_file.push(HEAD_FILE_NAME);
+    let head_file = rgit_dir.join(HEAD_FILE_NAME);
 
-    let _ =
-        File::create(head_file).map_err(|e| format!("failed to create Head file in {:?}", name))?;
+    let _ = File::create(head_file)
+        .map_err(|e| format!("failed to create HEAD file in {:?}: {}", name, e))?;
+
+    // Creates config file
+    let config_file = rgit_dir.join(CONFIG_FILE_NAME);
+    let _ = File::create(config_file)
+        .map_err(|e| format!("failed to create config file in {:?}: {}", name, e))?;
+
     println!("Initialized repo {:?}", name.clone());
     return Ok(());
 }
